44da39f4a4118e75cec9151afe90db70041c25cf
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-08 21:43:44 -0400

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,7 +1,7 @@
 <?php
 
-use yii\bootstrap\ActiveForm;
-use yii\helpers\Html;
+use humhub\compat\CActiveForm;
+use humhub\compat\CHtml;
 use humhub\models\Setting;
 use humhub\modules\twitter\controllers\AdminController;
 
@@ -10,7 +10,7 @@ use humhub\modules\twitter\controllers\AdminController;
 <div class="panel panel-default">
 	<div class="panel-heading"><?= Yii::t('TwitterModule.base', '<strong>Twitter</strong>'); ?></div>
 	<div class="panel-body">
-		<?= $form = ActiveForm::begin(['id' => 'twitter-settings-form']); ?>
+		<?= $form = CActiveForm::begin(['id' => 'twitter-settings-form']); ?>
 			<?= $form->errorSummary($model); ?>
 			<p class="help-block"><?= Yii::t('TwitterModule.base', 'eg:  "99999999"'); ?></p>
 			<div class="form-group">
@@ -18,8 +18,8 @@ use humhub\modules\twitter\controllers\AdminController;
 				<?= $form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'twitter')]); ?>
 			</div>
 			<p class="help-block"><?=Yii::t('TwitterModule.base', 'Widget positioning.'); ?></p>
-			<?= Html::submitButton(Yii::t('TwitterModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
+			<?= CHtml::submitButton(Yii::t('TwitterModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
 			<?= \humhub\widgets\DataSaved::widget(); ?>
-		<?php ActiveForm::end(); ?>
+		<?php CActiveForm::end(); ?>
 	</div>
 </div>
