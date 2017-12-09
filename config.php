0c84913f55ac406201672638591b2b9deb24e173
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-12-09 10:59:53 +0900

    Remove from Spaces till implement toggle switch

--- config.php
+++ config.php
@@ -15,14 +15,14 @@ return [
                 'addTwitterFrame'
             ]
         ],
-        [
-            'class' => \humhub\modules\space\widgets\Sidebar::className(),
-            'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\twitter\Events',
-                'addTwitterFrame'
-            ]
-        ],
+//        [
+//            'class' => \humhub\modules\space\widgets\Sidebar::className(),
+//            'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
+//            'callback' => [
+//                'humhub\modules\twitter\Events',
+//                'addTwitterFrame'
+//            ]
+//        ],
         [
             'class' => \humhub\modules\admin\widgets\AdminMenu::className(),
             'event' => \humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
